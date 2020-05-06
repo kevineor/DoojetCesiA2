ef74302a4f44d0ae9e2279fac1c0f8cf90736400
Audrey PIEDRA <audrey.piedra@viacesi.fr>
2020-05-06 18:09:01 +0200

    passage au nouveau dump bdd

--- DoojetCesiA2/Serveur/EM/Utilisateurs.cs
+++ DoojetCesiA2/Serveur/EM/Utilisateurs.cs
@@ -11,7 +11,7 @@ namespace DoojetCesiA2.EM
         //verify if the specified combo user/pass corresponds to an entry in the database
         public static string SELECT_BY_USERPASS(string user, string hashed_pass)
         {
-            return $"SELECT UID FROM utilisateurs WHERE username = '{user}' and pass = '{hashed_pass}';";
+            return $"SELECT UID FROM Users WHERE username = '{user}' and password = '{hashed_pass}';";
         }
 
         
