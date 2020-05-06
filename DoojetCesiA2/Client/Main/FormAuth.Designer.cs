f3d685f5cbcfd35fd52abba765103f399eecbb57
Audrey PIEDRA <audrey.piedra@viacesi.fr>
2020-05-06 18:12:50 +0200

    * au lieu de notes

--- DoojetCesiA2/Client/Main/FormAuth.Designer.cs
+++ DoojetCesiA2/Client/Main/FormAuth.Designer.cs
@@ -39,27 +39,24 @@
             // 
             // txt_username
             // 
-            this.txt_username.Location = new System.Drawing.Point(119, 87);
-            this.txt_username.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_username.Location = new System.Drawing.Point(89, 71);
             this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(132, 22);
+            this.txt_username.Size = new System.Drawing.Size(100, 20);
             this.txt_username.TabIndex = 0;
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(119, 131);
-            this.txt_password.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_password.Location = new System.Drawing.Point(89, 106);
             this.txt_password.Name = "txt_password";
-            this.txt_password.PasswordChar = '♫';
-            this.txt_password.Size = new System.Drawing.Size(132, 22);
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(100, 20);
             this.txt_password.TabIndex = 1;
             // 
             // btn_auth
             // 
-            this.btn_auth.Location = new System.Drawing.Point(72, 182);
-            this.btn_auth.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_auth.Location = new System.Drawing.Point(54, 148);
             this.btn_auth.Name = "btn_auth";
-            this.btn_auth.Size = new System.Drawing.Size(133, 28);
+            this.btn_auth.Size = new System.Drawing.Size(100, 23);
             this.btn_auth.TabIndex = 2;
             this.btn_auth.Text = "Se Connecter";
             this.btn_auth.UseVisualStyleBackColor = true;
@@ -68,11 +65,10 @@
             // lb_output
             // 
             this.lb_output.AutoSize = true;
-            this.lb_output.Location = new System.Drawing.Point(11, 146);
-            this.lb_output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lb_output.MaximumSize = new System.Drawing.Size(133, 0);
+            this.lb_output.Location = new System.Drawing.Point(8, 119);
+            this.lb_output.MaximumSize = new System.Drawing.Size(100, 0);
             this.lb_output.Name = "lb_output";
-            this.lb_output.Size = new System.Drawing.Size(0, 17);
+            this.lb_output.Size = new System.Drawing.Size(0, 13);
             this.lb_output.TabIndex = 3;
             this.lb_output.Click += new System.EventHandler(this.label1_Click);
             // 
@@ -81,9 +77,10 @@
             this.LabelAuthentification.AutoSize = true;
             this.LabelAuthentification.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LabelAuthentification.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelAuthentification.Location = new System.Drawing.Point(40, 24);
+            this.LabelAuthentification.Location = new System.Drawing.Point(30, 20);
+            this.LabelAuthentification.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LabelAuthentification.Name = "LabelAuthentification";
-            this.LabelAuthentification.Size = new System.Drawing.Size(197, 29);
+            this.LabelAuthentification.Size = new System.Drawing.Size(155, 22);
             this.LabelAuthentification.TabIndex = 4;
             this.LabelAuthentification.Text = "Authentification";
             this.LabelAuthentification.Click += new System.EventHandler(this.label1_Click_1);
@@ -91,25 +88,27 @@
             // LabelUsername
             // 
             this.LabelUsername.AutoSize = true;
-            this.LabelUsername.Location = new System.Drawing.Point(25, 87);
+            this.LabelUsername.Location = new System.Drawing.Point(19, 71);
+            this.LabelUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LabelUsername.Name = "LabelUsername";
-            this.LabelUsername.Size = new System.Drawing.Size(73, 17);
+            this.LabelUsername.Size = new System.Drawing.Size(55, 13);
             this.LabelUsername.TabIndex = 5;
             this.LabelUsername.Text = "Username";
             // 
             // LabelPassword
             // 
             this.LabelPassword.AutoSize = true;
-            this.LabelPassword.Location = new System.Drawing.Point(25, 131);
+            this.LabelPassword.Location = new System.Drawing.Point(19, 106);
+            this.LabelPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LabelPassword.Name = "LabelPassword";
-            this.LabelPassword.Size = new System.Drawing.Size(69, 17);
+            this.LabelPassword.Size = new System.Drawing.Size(53, 13);
             this.LabelPassword.TabIndex = 6;
             this.LabelPassword.Text = "Password";
             this.LabelPassword.Click += new System.EventHandler(this.label3_Click);
             // 
             // FormAuth
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.LabelPassword);
             this.Controls.Add(this.LabelUsername);
@@ -118,9 +117,8 @@
             this.Controls.Add(this.btn_auth);
             this.Controls.Add(this.txt_password);
             this.Controls.Add(this.txt_username);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormAuth";
-            this.Size = new System.Drawing.Size(277, 291);
+            this.Size = new System.Drawing.Size(208, 236);
             this.Load += new System.EventHandler(this.FormAuth_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
